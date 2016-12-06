3da318fa914dee37f5e952d3fbbab60fc8bcc65d
idoz <ido.ze2@gmail.com>
2016-12-06 18:24:58 +0200

    -

---
+++ Steve_1.0/Steve_1.0/Classes/ExtentionMethonds.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Steve_1._0
+{
+    public static class ExtentionMethonds
+    {
+        public static System.Drawing.Size Resize(this Size Org, double Modifier)
+        {
+            return new Size(Convert.ToInt32(Org.Width * Modifier),Convert.ToInt32(Org.Height * Modifier));
+        }
+        public static Point RelativeLocation(this Control cont,Control Parent)
+        {
+            Control c = cont;
+            Point retval = new Point(0, 0);
+            for (; c.Parent != Parent; c = c.Parent)
+            { retval.Offset(c.Location); }
+            return retval;
+        }
+    }
+}
