667f7adbf2224611ddd69215269e2a77d5219b96
idoz <ido.ze2@gmail.com>
2016-12-12 00:04:10 +0200

    -

---
+++ Steve_1.0/Steve_1.0/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steve_1._0
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            Location = new Point(300, 300);
+            StartPosition = FormStartPosition.Manual;
+            button2.Select();
+            KeyDown += Form2_KeyDown;
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyData)
+            {
+                case Keys.Enter:
+                    button2_Click(sender, e);
+                    break;
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = "You Scored: "+((int)Form1.Score).ToString()+".";
+            Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
