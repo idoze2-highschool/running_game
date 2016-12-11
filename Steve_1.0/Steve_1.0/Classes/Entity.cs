d40647eb56ca3b729fc7c4612863ce8c087ed2d9
idoz <ido.ze2@gmail.com>
2016-12-11 23:37:22 +0200

    -

---
+++ Steve_1.0/Steve_1.0/Classes/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Steve_1._0
+{
+    public class Entity:PictureBox
+    {
+        private string imageDir = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.IndexOf("Running Game") + ("Running Game").Length) + @"\Images\";
+        public string ImageDir { get { return imageDir; } set { imageDir = value; } }
+        //public static string GetImageDir() { return new Entity().ImageDir; }
+        public Entity()
+        {
+            BackColor = Color.Transparent;
+        }
+    }
+}
